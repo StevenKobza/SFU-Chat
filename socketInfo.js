972fec27169c701f013a9425fb178192d8abac27
Steven <32111227+KobzaS@users.noreply.github.com>
2018-11-23 09:14:46 -0800

    Lots of updates apparently

--- socketInfo.js
+++ socketInfo.js
@@ -1,15 +1,18 @@
-var express = require("express")();
+var express = require("express");
 var app = express();
 var path = require("path");
 var server = require("http").createServer(app);
-var io = require("../..")(server);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+var file = path.join(__dirname, 'frontEnd');
+
+console.log(file);
 
 server.listen(port, () => {
     console.log ("Server listening at port %d", port);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/frontEnd')));
 
 var numUsers = 0;
 
@@ -23,7 +26,44 @@ io.on("connection", (socket) => {
         });
     });
 
-    
+    socket.on("add user", (username) => {
+        if (addedUser) return;
+
+        //We store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit("login", {
+            numUsers: numUsers
+        });
+        socket.broadcast.emit("user joined", {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    socket.on("typing", () => {
+        socket.broadcast.emit("typing", {
+            username: socket.username
+        });
+    });
+
+    socket.on("stop typing", () => {
+        socket.broadcast.emit("stop typing", {
+            username: socket.username
+        });
+    });
+
+    socket.on("disconnect", () => {
+        if (addedUser) {
+            --numUsers;
+
+            socket.broadcast.emit("user left", {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
 });
 
 /*app.get("/", function(req, res) {
